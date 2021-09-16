23846ca6228f15d7317b56a555652fa1209d84b1
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-09-16 20:47:56 +0300

    formik's functions are used such as handlechange and handlesubmit

--- src/pages/SimpleRegister.js
+++ src/pages/SimpleRegister.js
@@ -1,30 +1,32 @@
 import React from 'react'
 import '../App.css';
-import {useFormik} from "formik";
+import { useFormik } from "formik";
 
 function SimpleRegister() {
-
     const formik = useFormik({
         initialValues: {
-            name : "",
-            email : "",
-            userName: "",
+            name: "",
+            email: "",
+            userName: ""
         }
-    })
+    });
+    console.log("formik :>>", formik);
+
+
 
     return (
         <div className="container">
             <h1>Simple Form</h1>
-            
-            <form className="formStyle">
-                <label  htmlFor="name">Name</label>
-                <input type="text" id="name" name="name"></input>
-                
-                <label  htmlFor="email">Email</label>
-                <input type="text" id="email" name="email"></input>
+
+            <form className="formStyle" onSubmit={formik.handleSubmit}>
+                <label htmlFor="name">Name</label>
+                <input type="text" id="name" name="name" onChange={formik.handleChange} value={formik.values.name}></input>
+
+                <label htmlFor="email">Email</label>
+                <input type="text" id="email" name="email" onChange={formik.handleChange} value={formik.values.email}></input>
 
                 <label htmlFor="userName">User name</label>
-                <input type="text" id="userName" name="userName"></input>
+                <input type="text" id="userName" name="userName" onChange={formik.handleChange} value={formik.values.userName}></input>
 
                 <button>Submit</button>
             </form>
@@ -33,4 +35,3 @@ function SimpleRegister() {
 }
 
 export default SimpleRegister
-
